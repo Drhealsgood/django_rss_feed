6a1880d17c1b3433aed6c65a17b33b518d2ff3c0
Luke <doclukemckee@gmail.com>
2013-08-27 20:43:24 +1200

    updated DB and views. Added basic Index template

--- rss_feed/views.py
+++ rss_feed/views.py
@@ -1,5 +1,16 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from rss_feed.models import Feed
 
 def index(request):
-    return HttpResponse("Hello World. You're at the rss_feed index.")
+    """
+    index lists the feeds we are tracking
+    """
+    feed_list = Feed.objects.all()
+    template = loader.get_template('index.html')
+    context = RequestContext(request, {
+        'feed_list': feed_list,
+    })
+    return HttpResponse(template.render(context))
